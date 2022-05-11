007fcb4e046eff5bbd7b50b8b7cf4e3ba2a9601f
Andrew Samuel <andrewsamuel@TechnologysMBP2.example.com>
2022-05-11 13:58:39 +0100

    If statement and ternary operators

--- com/etherofgodd/Main.java
+++ com/etherofgodd/Main.java
@@ -3,12 +3,24 @@ package com.etherofgodd;
 public class Main {
 
     public static  void main(String[] args) {
+//  If statements
 
-//        Logical Operators
-        boolean isAdult = true;
-        boolean isStudent = false;
+        int age  = 17;
 
-        System.out.println(isAdult  || isStudent);
+        if(age >= 18){
+            System.out.println("This is an adult.");
+        }else if(age >=16 && age <= 18){
+            System.out.println("I am almost and old man, pele...");
+        } else {
+            System.out.println("I am not yet an adult");
+        }
+
+//        ternary operators....
+
+
+        String message =  age >= 18 ? "Old man": "KAI";
+        System.out.println(message);
+
 
     }
 }
