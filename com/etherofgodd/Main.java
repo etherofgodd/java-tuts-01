5057f11152feb0793c75d6a6479b3a00fd0b0803
Andrew Samuel <andrewsamuel@TechnologysMBP2.example.com>
2022-05-11 10:29:38 +0100

    Arithmetic operation

--- com/etherofgodd/Main.java
+++ com/etherofgodd/Main.java
@@ -1,20 +1,41 @@
 package com.etherofgodd;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
-
 public class Main {
 
     public static  void main(String[] args) {
-        Date date = new Date();
-        String s = new String();
-        LocalDate localdate = LocalDate.now();
-        LocalDateTime localDateTime = LocalDateTime.now();
+    //  Arithmetic Operations
+        int ten = 10;
+        int two = 2;
+        int addition = ten + two;
+        int subtraction = ten - two;
+        int multipication = ten * two;
+        int division = ten / two;
+        int modulo = ten % two;
 
-        // Reserved Keywords
+        System.out.println(addition);
+        System.out.println("sub "+subtraction);
+        System.out.println(multipication);
+        System.out.println(division);
+        System.out.println(modulo);
 
-//        void, file, private, final, class, static
+
+
+//      Using the math class.
+
+        int math = Math.abs(-10);
+        double biggerNum = Math.max(10.2,2.3);
+        double smallerNum = Math.min(10.2,2.3);
+        double raiseTopow = Math.pow(3,3);
+
+//      casting
+        int squareRoot = (int) Math.sqrt(25);
+
+        System.out.println("from "+math);
+        System.out.println(biggerNum);
+        System.out.println(smallerNum);
+        System.out.println(raiseTopow);
+        System.out.println(squareRoot);
+
 
 
     }
