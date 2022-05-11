a6402e77050e3b36441bbcd38cb5930658712f55
Andrew Samuel <andrewsamuel@Technologys-MacBook-Pro-2.local>
2022-05-11 16:44:38 +0100

    DO while loops

--- com/etherofgodd/Main.java
+++ com/etherofgodd/Main.java
@@ -6,32 +6,22 @@ public class Main {
 
     public static  void main(String[] args) {
 
-        int[] numbers = {2,0,1,4,100,4,90,78,77};
-//
-//        for(int i = 0; i< numbers.length; i++){
-//            System.out.println(numbers[i]);
-//        }
-//
+//        while loops
 
+        int count = 0;
 
-//        Reverse order
-//        for(int i = numbers.length -1; i >= 0; i--){
-//            System.out.println(numbers[i]);
-//        }
-
-        //    enhanced arrays
-        for(int number: numbers){
-//            System.out.println(number);
+        while (count <= 20){
+            System.out.println("Count "+ count);
+            count++;
         }
 
-//        numbers.forI
-//        for (int i = numbers.length - 1; i >= 0; i--) {
 //
-//        }
+//        do while
+        do {
+            System.out.println("Count from the do "+count);
+            count++;
+        } while (count <= 20);
 
-        Arrays.stream(numbers).forEach(System.out::println);
     }
-
-
 
 }
