72859c76c2955419e66965876041368fca35d62c
Andrew Samuel <andrewsamuel@TechnologysMBP2.example.com>
2022-05-11 11:17:32 +0100

    Logical Operators ---real

--- com/etherofgodd/Main.java
+++ com/etherofgodd/Main.java
@@ -3,22 +3,12 @@ package com.etherofgodd;
 public class Main {
 
     public static  void main(String[] args) {
-//      comparison operators ---  most are referred to as boolean expressions.
-        int khalidAge = 18;
-        int mariamAge = 20;
 
-        boolean isKhalidOlderThanMariam = khalidAge > mariamAge;
-        boolean isKhalidYoungerThanMariam = khalidAge < mariamAge;
-        boolean isKhalidSameAgeWithMariam = khalidAge == mariamAge;
-//        boolean isKhalidOlderThanMariam = khalidAge > mariamAge;
+//        Logical Operators
+        boolean isAdult = true;
+        boolean isStudent = false;
 
-        System.out.println("isKhalidOlderThanMariam" + isKhalidOlderThanMariam);
-        System.out.println("isKhalidYoungerThanMariam "+isKhalidYoungerThanMariam);
-        System.out.println("isKhalidSameAgeWithMariam" + isKhalidSameAgeWithMariam);
-
-        System.out.println(khalidAge>= mariamAge);
-        System.out.println(khalidAge<= mariamAge);
-        System.out.println(khalidAge!= mariamAge);
+        System.out.println(isAdult  || isStudent);
 
     }
 }
