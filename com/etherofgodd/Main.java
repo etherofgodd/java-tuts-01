3bfbcb8001eff643bcdd6171ab008c479d33d89f
Andrew Samuel <andrewsamuel@TechnologysMBP2.example.com>
2022-05-11 08:55:48 +0100

    Primitive types

--- com/etherofgodd/Main.java
+++ com/etherofgodd/Main.java
@@ -1,10 +1,41 @@
 package com.etherofgodd;
 
+import java.util.Date;
+
 public class Main {
 
     public static  void main(String[] args){
-        // single line write some comments
-        /* Multi-line comments. */
-//        System.out.println("Hooray! My first java application");
+    //  primitive data type; int long double
+        byte theByte = -123;
+        short theShort = 3933;
+        int theInt = 100;
+        long theLong = 3131301301301301313L;
+//        above 4 allows to store whole numbers;
+
+//        Below for decimals
+        float theFloat = 2.12F;
+        double doublePi = 3.142;
+
+        boolean isBroke = true;
+
+//        characters
+
+        char nameInitials = 'E';
+
+        System.out.println(nameInitials);;
+        System.out.println(theByte);;
+        System.out.println(theFloat);;
+        System.out.println(isBroke);;
+        System.out.println(doublePi);;
+        String alias = "ether of godd";
+
+        Date date = new Date();
+        System.out.println(date);
+
+//        underscores to visualize numbers
+        int amountOf = 3_000_000;
+        System.out.println(amountOf);
+
+
     }
 }
