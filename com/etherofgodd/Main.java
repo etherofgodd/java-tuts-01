2de06ad6875cdf06556ac32e273880a21107cf02
Andrew Samuel <andrewsamuel@TechnologysMBP2.example.com>
2022-05-11 16:28:47 +0100

    For loops

--- com/etherofgodd/Main.java
+++ com/etherofgodd/Main.java
@@ -5,25 +5,33 @@ import java.util.Arrays;
 public class Main {
 
     public static  void main(String[] args) {
-//    Arrays
 
-//        one way of creating and initializing an array;
-        int [] numbers = {2,3,4,5,6,7};
-        String [] names = {"ALI", "MARIA"};
-
-//        another way of doing the same;
-//        int [] numbers = new int[4];
-//        numbers[0] = 2;
-//        numbers[1]=4;
-//        numbers[2]=90;
-//        numbers[3]=33;
-
-        System.out.println(Arrays.toString(numbers));
-        System.out.println(numbers.length);
-        System.out.println(names.length);
+        int[] numbers = {2,0,1,4,100,4,90,78,77};
+//
+//        for(int i = 0; i< numbers.length; i++){
+//            System.out.println(numbers[i]);
+//        }
+//
 
 
-//        Arrays and indexes....
-//        [0,1,2,3,4,5,6,7,8,9,10,11,...]
+//        Reverse order
+//        for(int i = numbers.length -1; i >= 0; i--){
+//            System.out.println(numbers[i]);
+//        }
+
+        //    enhanced arrays
+        for(int number: numbers){
+//            System.out.println(number);
+        }
+
+//        numbers.forI
+//        for (int i = numbers.length - 1; i >= 0; i--) {
+//
+//        }
+
+        Arrays.stream(numbers).forEach(System.out::println);
     }
+
+
+
 }
