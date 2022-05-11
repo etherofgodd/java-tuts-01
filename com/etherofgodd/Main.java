6ded3d1203e2c7a06b9ea6efe8e540cf8c76ce33
Andrew Samuel <andrewsamuel@TechnologysMBP2.example.com>
2022-05-11 10:44:49 +0100

    Logical Operators

--- com/etherofgodd/Main.java
+++ com/etherofgodd/Main.java
@@ -3,40 +3,22 @@ package com.etherofgodd;
 public class Main {
 
     public static  void main(String[] args) {
-    //  Arithmetic Operations
-        int ten = 10;
-        int two = 2;
-        int addition = ten + two;
-        int subtraction = ten - two;
-        int multipication = ten * two;
-        int division = ten / two;
-        int modulo = ten % two;
+//      comparison operators ---  most are referred to as boolean expressions.
+        int khalidAge = 18;
+        int mariamAge = 20;
 
-        System.out.println(addition);
-        System.out.println("sub "+subtraction);
-        System.out.println(multipication);
-        System.out.println(division);
-        System.out.println(modulo);
+        boolean isKhalidOlderThanMariam = khalidAge > mariamAge;
+        boolean isKhalidYoungerThanMariam = khalidAge < mariamAge;
+        boolean isKhalidSameAgeWithMariam = khalidAge == mariamAge;
+//        boolean isKhalidOlderThanMariam = khalidAge > mariamAge;
 
+        System.out.println("isKhalidOlderThanMariam" + isKhalidOlderThanMariam);
+        System.out.println("isKhalidYoungerThanMariam "+isKhalidYoungerThanMariam);
+        System.out.println("isKhalidSameAgeWithMariam" + isKhalidSameAgeWithMariam);
 
-
-//      Using the math class.
-
-        int math = Math.abs(-10);
-        double biggerNum = Math.max(10.2,2.3);
-        double smallerNum = Math.min(10.2,2.3);
-        double raiseTopow = Math.pow(3,3);
-
-//      casting
-        int squareRoot = (int) Math.sqrt(25);
-
-        System.out.println("from "+math);
-        System.out.println(biggerNum);
-        System.out.println(smallerNum);
-        System.out.println(raiseTopow);
-        System.out.println(squareRoot);
-
-
+        System.out.println(khalidAge>= mariamAge);
+        System.out.println(khalidAge<= mariamAge);
+        System.out.println(khalidAge!= mariamAge);
 
     }
 }
