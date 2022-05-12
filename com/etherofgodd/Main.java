2eb2d14207923afc5b1f7e7aff581e0fc95f92b4
Andrew Samuel <andrewsamuel@Technologys-MacBook-Pro-2.local>
2022-05-12 11:17:03 +0100

    Java classes and Objects

--- com/etherofgodd/Main.java
+++ com/etherofgodd/Main.java
@@ -1,38 +1,53 @@
 package com.etherofgodd;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 public class Main {
 
     public static  void main(String[] args) {
 
-//    Methods
+//        classes and objects
 
-//        Built in methods
-        System.out.println("Hurray");
-        String brand = "Samsung";
-        System.out.println(brand.toUpperCase());
-        System.out.println(brand.startsWith("S"));
+        Lens lensOne = new Lens("Panasonic", "85mm", false);
+        Lens lensTwo = new Lens("Sony", "95mm", true);
+        Lens lensThree = new Lens("Cannon", "103mm", true);
 
-//        custom methods
-
-        char[] letters = {'A', 'B', 'C','D','D','E','7'};
-
-        System.out.println(countOccurrences(letters, 'D'));
-
+        System.out.println(lensOne.brand);
+        System.out.println(lensTwo.focalLength);
+        System.out.println(lensThree.isPrime);
     }
 
-    public static  int countOccurrences(char[] letters,  char searchLetter){
-        System.out.println(Arrays.toString(letters));
-        System.out.println(searchLetter);
+    public static  class Lens {
+        String brand;
+        String focalLength;
+        boolean isPrime;
 
-        int count = 0;
-        for (char letter: letters){
-            if(letter == searchLetter){
-                count++;
-            }
+        Lens(String brand, String focalLength, boolean isPrime){
+            this.brand = brand;
+            this.focalLength = focalLength;
+            this.isPrime = isPrime;
         }
-        return  count;
+
+        Passport ukPassport = new Passport(
+                "012",
+                LocalDate.of(2024,12,29));
+
+        Passport usPassport = new Passport(
+                "013",
+                LocalDate.of(2025,3,21));
+    }
+
+    static  class Passport {
+        String number;
+        LocalDate expiryDate;
+
+        Passport(String number, LocalDate expiryDate){
+            this.number = number;
+            this.expiryDate = expiryDate;
+        }
+
+
     }
 
 }
